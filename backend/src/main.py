d9088a10975569a066c8f77cdc4887757e1f72b1
Ignacio Basagoiti <ignacio.bas.car@gmail.com>
2023-09-21 19:35:15 +0200

    added first get endpoint

--- backend/src/main.py
+++ backend/src/main.py
@@ -9,5 +9,11 @@ def initialize_backend_application() -> fastapi.FastAPI:
 
 app: fastapi.FastAPI = initialize_backend_application()
 
+
+@app.get("/hello_world")
+def hello_world():
+    return {"hello": "world"}
+
+
 if __name__ == "__main__":
     uvicorn.run(app="main.app")
