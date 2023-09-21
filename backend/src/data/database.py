8db9ed84f6039f322cf2bd1fcc23e3fc638fcbf3
Ignacio Basagoiti <ignacio.basagoiti@cytel.com>
2023-09-21 17:45:54 +0200

    updated dependencies on cy-backend

--- backend/src/data/database.py
+++ backend/src/data/database.py
@@ -5,7 +5,9 @@ from sqlalchemy.orm import Session, sessionmaker
 SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal: Session = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine
+)
 
 SqlAlchemyBase = declarative_base()
 
