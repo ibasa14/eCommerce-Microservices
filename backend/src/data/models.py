df70073c07073bf634e104f2774b9151390ec1b4
Ignacio Basagoiti <ignacio.basagoiti@cytel.com>
2023-09-22 18:50:59 +0200

    added annotations to models

--- backend/src/data/models.py
+++ backend/src/data/models.py
@@ -1,4 +1,5 @@
 import enum
+from typing import Any
 
 from sqlalchemy import (
     Boolean,
@@ -12,84 +13,110 @@ from sqlalchemy import (
     Integer,
     String,
 )
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, Relationship
 
 from .database import SqlAlchemyBase  # type: ignore
 
 
 class RoleType(enum.Enum):
-    admin = "admin"
-    client = "client"
+    admin: str = "admin"
+    client: str = "client"
 
 
 class CategoryType(enum.Enum):
-    meat = "meat"
-    seafood = "seafood"
-    beverage = "beverage"
-    nonfood = "nonfood"
-    bakery = "bakery"
+    meat: str = "meat"
+    seafood: str = "seafood"
+    beverage: str = "beverage"
+    nonfood: str = "nonfood"
+    bakery: str = "bakery"
 
 
 class User(SqlAlchemyBase):  # type: ignore
-    __tablename__ = "users"
+    __tablename__: str = "users"
 
-    id_user = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String, nullable=False, unique=True)
-    email = Column(String, nullable=False, unique=True)
-    hashed_password = Column(String, nullable=False)
-    is_active = Column(Boolean, default=True)
-    role_id = Column(Integer, ForeignKey("roles.id_role"))
-    role = relationship("Role")
-    orders = relationship("Order", back_populates="user")
+    id_user: Column[Integer] = Column(
+        Integer, primary_key=True, autoincrement=True
+    )
+    name: Column[String] = Column(String, nullable=False, unique=True)
+    email: Column[String] = Column(String, nullable=False, unique=True)
+    hashed_password: Column[String] = Column(String, nullable=False)
+    is_active: Column[Boolean] = Column(Boolean, default=True)
+    role_id: Column[Integer] = Column(Integer, ForeignKey("roles.id_role"))
+    role: Relationship[Any] = relationship("Role")
+    orders: Relationship[Any] = relationship("Order", back_populates="user")
 
 
 class Role(SqlAlchemyBase):  # type: ignore
-    __tablename__ = "roles"
-    id_role = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(Enum(RoleType))
+    __tablename__: str = "roles"
+    id_role: Column[Integer] = Column(
+        Integer, primary_key=True, autoincrement=True
+    )
+    name: Column[Enum] = Column(Enum(RoleType))
 
 
 class Product(SqlAlchemyBase):  # type: ignore
-    __tablename__ = "products"
+    __tablename__: str = "products"
 
-    id_product = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String, nullable=False)
-    description = Column(String)
-    picture = Column(String, default="not_defined.png")
-    price = Column(Float, nullable=False)
-    stock = Column(Integer, default=0)
-    category_id = Column(Enum(CategoryType))
-    category = relationship("Category", back_populates="products")
+    id_product: Column[Integer] = Column(
+        Integer, primary_key=True, autoincrement=True
+    )
+    name: Column[String] = Column(String, nullable=False)
+    description: Column[String] = Column(String)
+    picture: Column[String] = Column(String, default="not_defined.png")
+    price: Column[Float] = Column(Float, nullable=False)
+    stock: Column[Integer] = Column(Integer, default=0)
+    category_id: Column[Enum] = Column(Enum(CategoryType))
+    category: Relationship[Any] = relationship(
+        "Category", back_populates="products"
+    )
 
 
 class Category(SqlAlchemyBase):  # type: ignore
-    __tablename__ = "categories"
-    id_category = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(Enum(CategoryType))
-    products = relationship("Product", back_populates="category")
+    __tablename__: str = "categories"
+    id_category: Column[Integer] = Column(
+        Integer, primary_key=True, autoincrement=True
+    )
+    name: Column[Enum] = Column(Enum(CategoryType))
+    products: Relationship[Any] = relationship(
+        "Product", back_populates="category"
+    )
 
 
 class Order(SqlAlchemyBase):  # type: ignore
-    __tablename__ = "orders"
+    __tablename__: str = "orders"
 
-    id_order = Column(Integer, primary_key=True, autoincrement=True)
-    user_id = Column(Integer, ForeignKey("users.id_user"))
-    user = relationship("User", back_populates="orders")
-    date = Column(DateTime(timezone=True), server_default=func.now())
-    total = Column(Float, default=0)
-    order_detail = relationship("OrderDetail", back_populates="order")
+    id_order: Column[Integer] = Column(
+        Integer, primary_key=True, autoincrement=True
+    )
+    user_id: Column[Integer] = Column(Integer, ForeignKey("users.id_user"))
+    user: Relationship[Any] = relationship("User", back_populates="orders")
+    date: Column[DateTime] = Column(
+        DateTime(timezone=True), server_default=func.now()
+    )
+    total: Column[Float] = Column(Float, default=0)
+    order_detail: Relationship[Any] = relationship(
+        "OrderDetail", back_populates="order"
+    )
 
 
 class OrderDetail(SqlAlchemyBase):  # type: ignore
-    __tablename__ = "order_details"
+    __tablename__: str = "order_details"
 
-    id_order_detail = Column(Integer, primary_key=True, autoincrement=True)
-    product_id = Column(Integer, ForeignKey("products.id_product"))
-    product = relationship("Product", back_populates="order_detail")
-    order_id = Column(Integer, ForeignKey("orders.id_order"))
-    order = relationship("Order", back_populates="order_details")
-    quantity = Column(Integer, nullable=False)
-    total = Column(Float, default=0)
-    __table_args__ = (
+    id_order_detail: Column[Integer] = Column(
+        Integer, primary_key=True, autoincrement=True
+    )
+    product_id: Column[Integer] = Column(
+        Integer, ForeignKey("products.id_product")
+    )
+    product: Relationship[Any] = relationship(
+        "Product", back_populates="order_detail"
+    )
+    order_id: Column[Integer] = Column(Integer, ForeignKey("orders.id_order"))
+    order: Relationship[Any] = relationship(
+        "Order", back_populates="order_details"
+    )
+    quantity: Column[Integer] = Column(Integer, nullable=False)
+    total: Column[Float] = Column(Float, default=0)
+    __table_args__: CheckConstraint = (
         CheckConstraint(quantity > 0, name="check_quantity_positive"),
     )
